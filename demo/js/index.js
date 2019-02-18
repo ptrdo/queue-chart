82f7ef667347fa10ff87ac2a574ac8dd35b60ac4
Peter Sylwester <ptrdo@users.noreply.github.com>
2019-02-18 10:34:15 -0800

    Checkpoint

--- demo/js/index.js
+++ demo/js/index.js
@@ -18,7 +18,9 @@ class Index {
       chart.render();
     });
 
-    rootElement.appendChild(button);
+    // rootElement.appendChild(button);
+    document.querySelector("[itemid=QueueChart]").appendChild(button);
+
     console.log("The Index module has been rendered!", $(button).get(0));
 
     setTimeout(function () {
